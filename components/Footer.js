46440bbc929bdb7503cac297eaf23970c8cdb7a3
farzanatabassum <farzanatabassum531@gmail.com>
2022-10-25 16:12:11 +0600

    ui:#1 fixed color of footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,24 +9,24 @@ import { FiLinkedin } from 'react-icons/fi';
 const Footer = () => {
   return (
     <div>
-      <footer className="text-gray-600 body-font bg-slate-300">
+      <footer className="text-gray-600 body-font bg-slate-500">
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <Link href={'/'}><a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-      <span className="ml-3 text-xl">Regalia</span>
+      <span className="ml-3 text-xl text-white">Regalia</span>
     </a></Link>
-    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2022 Regalia 
+    <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-white sm:py-2 sm:mt-0 mt-4">© 2022 Regalia 
     </p>
     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <a className="text-gray-500">
+      <a className="text-white">
       <RiFacebookFill className='text-2xl'/>
       </a>
-      <a className="ml-3 text-gray-500">
+      <a className="ml-3 text-white">
         <GrTwitter className='text-2xl'/>
       </a>
-      <a className="ml-3 text-gray-500">
+      <a className="ml-3 text-white">
         <BsInstagram className='text-2xl'/>
       </a>
-      <a className="ml-3 text-gray-500">
+      <a className="ml-3 text-white">
        <FiLinkedin className='text-2xl'/>
       </a>
     </span>

6d8c36def0bc3bc9e0fd5349d829c5f6d10dc5bd
farzanatabassum <farzanatabassum531@gmail.com>
2022-10-25 13:02:11 +0600

    style: #2 fixed formatting code

--- pages/signup.js
+++ pages/signup.js
@@ -4,70 +4,70 @@ import Link from 'next/link';
 const Signup = () => {
   return (
     <div>
-      <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div class="w-full max-w-md space-y-8">
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
           <div>
-            <h1 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            <h1 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Regalia
             </h1>
-            <h2 class="mt-6 text-center text-base font-bold tracking-tight text-gray-900">
+            <h2 className="mt-6 text-center text-base font-bold tracking-tight text-gray-900">
               SIGN UP FOR AN ACCOUNT
             </h2>
-            <p class="mt-2 text-center text-sm text-gray-600">
+            <p className="mt-2 text-center text-sm text-gray-600">
                 <span> Or </span>
               
               <Link href="/login">
                 <a
                   href="#"
-                  class="font-medium text-gray-800 hover:text-gray-600"
+                  className="font-medium text-gray-800 hover:text-gray-600"
                 >
                  LOGIN
                 </a>
               </Link>
             </p>
           </div>
-          <form class="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" value="true" />
-            <div class="-space-y-px rounded-md shadow-sm">
+            <div className="-space-y-px rounded-md shadow-sm">
               <div>
-                <label for="name" class="sr-only">
+                <label htmlFor="name" className="sr-only">
                   Name
                 </label>
                 <input
                   id="name"
                   name="name"
                   type="text"
-                  autocomplete="name"
+                  autoComplete="name"
                   required
-                  class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
                   placeholder="Your name"
                 />
               </div>
               <div>
-                <label for="email-address" class="sr-only">
+                <label htmlFor="email-address" className="sr-only">
                   Email address
                 </label>
                 <input
                   id="email-address"
                   name="email"
                   type="email"
-                  autocomplete="email"
+                  autoComplete="email"
                   required
-                  class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
                   placeholder="Email address"
                 />
               </div>
               <div>
-                <label for="password" class="sr-only">
+                <label htmlFor="password" className="sr-only">
                   Password
                 </label>
                 <input
                   id="password"
                   name="password"
                   type="password"
-                  autocomplete="current-password"
+                  autoComplete="current-password"
                   required
-                  class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
                   placeholder="Password"
                 />
               </div>
@@ -76,7 +76,7 @@ const Signup = () => {
             <div>
               <button
                 type="submit"
-                class="group relative flex w-full justify-center rounded-md border border-transparent bg-gray-800 py-2 px-4 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-gray-800 py-2 px-4 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
               >
                 SIGN UP
               </button>

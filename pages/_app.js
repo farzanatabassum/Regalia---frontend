7cfb47de9598af9cfd644d5b8b718516c57c1186
farzanatabassum <farzanatabassum531@gmail.com>
2022-10-25 12:07:07 +0600

    ui:#2 created signup form

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import Signup from './signup'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Signup />
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp

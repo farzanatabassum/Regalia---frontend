a5c9f22aadc545266d416cb30db0469744074bc3
farzanatabassum <farzanatabassum531@gmail.com>
2022-10-26 20:49:16 +0600

    ui:#5 created components for single product page

---
+++ components/SingleProduct.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const SingleProduct = ({img,category,brand,fabric,condition,size,price}) => {
+  return (
+    <div>
+        <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">    
+            <img
+              alt="ecommerce"
+              className="lg:w-1/2 lg:h-auto object-cover w-7/12 object-top rounded"
+              src={img}
+            />
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-60 mt-6 lg:mt-0 ">
+            <h1 className="text-gray-900  text-3xl title-font font-medium mb-3 ">
+             Description
+              </h1>
+              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
+                <span className='font-bold'>Category:</span> {category}
+              </h2>
+              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
+              <span className='font-bold'>Brand:</span> {brand}
+              </h2>
+              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
+              <span className='font-bold'>Fabric:</span> {fabric}
+              </h2>
+              <h2 className="mb-2 text-lg title-font text-gray-500 tracking-widest">
+              <span className='font-bold'>Condition:</span> {condition}
+              </h2>
+              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
+              <span className='font-bold'>Size:</span> {size}
+              </h2>
+              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
+              <span className='font-bold'>Price:</span> {price}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+    </div>
+  )
+}
+
+export default SingleProduct
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-// import Image from 'next/image';
+import SingleProduct from '../../components/SingleProduct';
 
 const Post = () => {
   const router = useRouter();
@@ -7,43 +7,7 @@ const Post = () => {
 
   return (
     <>
-      <section className="text-gray-600 body-font overflow-hidden">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            {/* <Image alt="ecommerce"
-              className="lg:w-1/2  lg:h-auto  object-cover w-7/12 object-top rounded"
-              src="/dress1.jpg" width='380px' height='300px'></Image> */}
-            <img
-              alt="ecommerce"
-              className="lg:w-1/2  lg:h-auto  object-cover w-7/12 object-top rounded"
-              src="/dress1.jpg"
-            />
-            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-60 mt-6 lg:mt-0 ">
-            <h1 className="text-gray-900  text-3xl title-font font-medium mb-3 ">
-             Description
-              </h1>
-              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
-                <span className='font-bold'>Category:</span> Dress
-              </h2>
-              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
-              <span className='font-bold'>Brand:</span> Yellow
-              </h2>
-              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
-              <span className='font-bold'>Fabric:</span> Diamond Georgette
-              </h2>
-              <h2 className="mb-2 text-lg title-font text-gray-500 tracking-widest">
-              <span className='font-bold'>Condition:</span> New with tag
-              </h2>
-              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
-              <span className='font-bold'>Size:</span> M
-              </h2>
-              <h2 className=" mb-2 text-lg title-font text-gray-500 tracking-widest">
-              <span className='font-bold'>Price:</span> Tk 2000
-              </h2>
-            </div>
-          </div>
-        </div>
-      </section>
+     <SingleProduct img='/dress1.jpg' category='Dress' brand='Yellow' fabric='Diamond Georgette' condition='New with tag' size='M' price='TK 2000'/>
     </>
   );
 };

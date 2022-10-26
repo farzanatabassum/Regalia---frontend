c608e5bd2ec6169a0384a0dd6cf88d8908784991
farzanatabassum <farzanatabassum531@gmail.com>
2022-10-26 17:36:10 +0600

    ui:#2 removed a line

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-       <h1>REGALIA</h1>
+     
 
     </div>
   )

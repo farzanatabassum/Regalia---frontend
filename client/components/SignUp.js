fc6ba35bfeb8cb39e0d1402c6462b32cc192deac
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-11-01 10:56:13 +0600

    perf:#10 connecting the signup UI with backend

--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link';
 
 
 const SignUp = () => {
+  // const [name,setName]=useState()
+  // const [email,setEmail]=useState()
+  // const [password,setPassword]=useState()
+  // const handleChange=(e)=>{
+  //   if(e.target.name==='name'){
+  //     setName(e.target.value)
+  //   }
+  //   else if(e.target.name==='email'){
+  //     setEmail(e.target.value)
+  //   }
+  //   else if(e.target.name==='password'){
+  //     setPassword(e.target.value)
+  //   }
+
+  // }
+  // const handleSubmit=async(e)=>{
+  //   e.preventDefault()
+  //   const userData={name,email,password}
+  //   let res=await fetch("http://localhost:3000/api/users/signup",{
+  //     method:'POST',
+  //     headers:{
+  //       'Content-Type':'application/json',
+
+  //     },
+  //     body: JSON.stringify(userData),
+  //   })
+  //   let response=await res.json()
+  //   console.log(response)
+  //   setName('')
+  //   setEmail('')
+  //   setPassword('')
+  // }
   return (
     <div>
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -27,7 +59,8 @@ const SignUp = () => {
               </Link>
             </p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          {/* <form onSubmit={handleSubmit} className="mt-8 space-y-6" action="#" method="POST"> */}
+          <form  className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" value="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
@@ -35,7 +68,7 @@ const SignUp = () => {
                 <label htmlFor="name" className="sr-only">
                   Name
                 </label>
-                <input
+                <input 
                   id="name"
                   name="name"
                   type="text"
@@ -43,21 +76,26 @@ const SignUp = () => {
                   required
                   className=" mb-5 relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
                   placeholder="Your name"
+                  // onChange={handleChange}
+                  value={name}
                 />
               </div>
               <div>
               <h2 className='mb-2'>Email Address</h2>
-                <label htmlFor="email-address" className="sr-only">
+                <label htmlFor="email" className="sr-only">
                   Email address
                 </label>
                 <input
-                  id="email-address"
+                  id="email"
                   name="email"
                   type="email"
                   autoComplete="email"
                   required
                   className=" mb-5 relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
                   placeholder="Email address"
+                  // onChange={handleChange}
+                  value={email}
+
                 />
               </div>
               <div>
@@ -73,6 +111,8 @@ const SignUp = () => {
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
                   placeholder="Password"
+                  // onChange={handleChange}
+                  value={password}
                 />
               </div>
             </div>

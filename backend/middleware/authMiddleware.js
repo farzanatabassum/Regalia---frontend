84f3368921d0e57acf9dd50956d7926f539afc50
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-11-06 22:13:22 +0600

    perf:#9 added auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,39 @@
+const jwt=require('jsonwebtoken')
+const asyncHandler=require('express-async-handler')
+const User=require('../models/user.model')
+
+const protect=asyncHandler(async(req,res,next)=>{
+    let token
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try{
+            //get token from the user
+            token=req.headers.authorization.split('')[1]
+            //Verify token
+            const decoded=jwt.verify(token,process.env.JWT_SECRET)
+            //get user from the token
+            const user=await User.findById(decoded.id).select('-password')
+            if(!user){
+                res.status(404)
+                throw new Error('No user found')
+            }
+            req.user = user
+
+            next()
+        
+
+        }
+        catch(error){
+            console.log(error)
+            res.status(401)
+            throw new Error('Not authorized')
+
+        }
+    }
+    //token not found
+    if(!token){
+        req.status(401)
+        throw new Error('Not authorized, no token')
+    }
+
+})
+module.exports={protect}

7109ef7fa59bab046e9e237d3405a8ec6315c16b
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-11-12 22:30:31 +0600

    perf:#17 create edit product route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const {createProduct,getProduct,deleteProduct}=require('../controllers/product.controller')
+const {createProduct,getProduct,editProduct,deleteProduct}=require('../controllers/product.controller')
 router.route('/create').post(protect,createProduct)
 router.route('/read').get(protect,getProduct)
+router.route('/editProduct').put(protect,editProduct)
 router.route('/deleteProduct').delete(protect,deleteProduct)
 module.exports = router;

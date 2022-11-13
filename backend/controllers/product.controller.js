fb46ffe556d56449d3fff0f4492edec2418ebe29
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-11-13 10:41:06 +0600

    perf:#17 modified product routes

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,159 +1,179 @@
-const Product=require('../models/product.model')
-const asyncHandler=require('express-async-handler')
-const cloudinary = require("../config/cloudinary");
-
+const Product = require('../models/product.model');
+const asyncHandler = require('express-async-handler');
+const cloudinary = require('../config/cloudinary');
 
 //api/products/create
 //post req
 //private
-const createProduct=asyncHandler(async(req,res)=>{
-  const file=req.files.image
-  const result = await cloudinary.uploader.upload(file.tempFilePath)
-  console.log(result)
-    const {category,brand,fabric,size,condition,gender,originPrice, sellingPrice,tags}=req.body
+const createProduct = asyncHandler(async (req, res) => {
+  //uploading image in cloudinary
+  const file = req.files.image;
+  const result = await cloudinary.uploader.upload(file.tempFilePath);
+  console.log(result);
+  const {
+    category,
+    brand,
+    fabric,
+    size,
+    condition,
+    gender,
+    originPrice,
+    sellingPrice,
+    tags,
+  } = req.body;
 
-    //Any Input field is empty
-    if(!file||!category||!brand||!fabric||!size||!condition||!gender||!originPrice||!sellingPrice||!tags){
-        res.status(400)
-        throw new Error("Please add all fields")
-    }
-    const product=await Product.create({
-        image:result.url,
-        category,
-        brand,
-        fabric,
-        size,
-        condition,
-        gender,
-        originPrice, 
-        sellingPrice,
-        tags,
-        user:req.user.id,
-    })
-    res.status(200).json(product)
-})
+  //Any Input field is empty
+  if (
+    !file ||
+    !category ||
+    !brand ||
+    !fabric ||
+    !size ||
+    !condition ||
+    !gender ||
+    !originPrice ||
+    !sellingPrice ||
+    !tags
+  ) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+  const product = await Product.create({
+    image: result.url,
+    category,
+    brand,
+    fabric,
+    size,
+    condition,
+    gender,
+    originPrice,
+    sellingPrice,
+    tags,
+    user: req.user.id,
+  });
+  res.status(200).json(product);
+});
 
 //api/products/read
 //get req
 //private
-const getProduct=asyncHandler(async(req,res)=>{
-    const products = await Product.find({ user: req.user.id })
+//User gets all product
+const getProduct = asyncHandler(async (req, res) => {
+  const products = await Product.find({ user: req.user.id });
 
-    res.status(200).json(products)
-
-})
+  res.status(200).json(products);
+});
 //api/products/editProduct?id=id&url=imageUrl
 //put req
 //private
-const editProduct=asyncHandler(async(req,res)=>{
+//User can update product
+const editProduct = asyncHandler(async (req, res) => {
   let product = await Product.findById(req.query.id);
 
   if (!product) {
-    res.status(400)
-    throw new Error('Product not found')
+    res.status(400);
+    throw new Error('Product not found');
   }
 
-   // Check for user
-   if (!req.user) {
-    res.status(401)
-    throw new Error('User not found')
+  // Check for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error('User not found');
   }
   //Getting image name from image url
-  const imageUrl=req.query.url
-  const imageUrlArray=imageUrl.split('/')
-  const imageSplit=imageUrlArray[imageUrlArray.length-1]
-  const imageName=imageSplit.split('.')[0]
-  console.log(imageName)
+  const imageUrl = req.query.url;
+  const imageUrlArray = imageUrl.split('/');
+  const imageSplit = imageUrlArray[imageUrlArray.length - 1];
+  const imageName = imageSplit.split('.')[0];
+  console.log(imageName);
 
-   //Deleting image from cloudinary
-  const resultImage=await cloudinary.uploader.destroy(imageName)
-  res.status(200).json({message:resultImage})
+  //Deleting image from cloudinary
+  const resultImage = await cloudinary.uploader.destroy(imageName);
+  res.status(200).json({ message: resultImage });
 
   //inserting new image in cloudinary
-  const file=req.files.image
-  const result = await cloudinary.uploader.upload(file.tempFilePath)
-  console.log(result)
-  const data={
-    image:result.url||product.image,
-        category:req.body.category||product.category,
-        brand:req.body.brand||product.brand,
-        fabric:req.body.fabric||product.fabric,
-        size:req.body. size||product. size,
-        condition:req.body.condition||product.condition,
-        gender:req.body.gender||product.gender,
-        originPrice:req.body.originPrice||product.originPrice, 
-        sellingPrice:req.body.sellingPrice||product.sellingPrice,
-        tags:req.body. tags||product. tags,
-  }
+  const file = req.files.image;
+  const result = await cloudinary.uploader.upload(file.tempFilePath);
+  console.log(result);
+  const data = {
+    image: result.url || product.image,
+    category: req.body.category || product.category,
+    brand: req.body.brand || product.brand,
+    fabric: req.body.fabric || product.fabric,
+    size: req.body.size || product.size,
+    condition: req.body.condition || product.condition,
+    gender: req.body.gender || product.gender,
+    originPrice: req.body.originPrice || product.originPrice,
+    sellingPrice: req.body.sellingPrice || product.sellingPrice,
+    tags: req.body.tags || product.tags,
+  };
   //update product
-  
-  const updatedProduct = await Product.findByIdAndUpdate(req.query.id, data,{
+
+  const updatedProduct = await Product.findByIdAndUpdate(req.query.id, data, {
     new: true,
-  })
-  res.status(200).json(updatedProduct)
-
-
-})
+  });
+  res.status(200).json(updatedProduct);
+});
 
 //api/products/deleteProduct?id=id&url=imageUrl
 //delete req
 //private
-const deleteProduct=asyncHandler(async(req,res)=>{
+//User can delete product
+const deleteProduct = asyncHandler(async (req, res) => {
   let product = await Product.findById(req.query.id);
 
   if (!product) {
-    res.status(400)
-    throw new Error('Product not found')
+    res.status(400);
+    throw new Error('Product not found');
   }
-   // Check for user
-   if (!req.user) {
-    res.status(401)
-    throw new Error('User not found')
+  // Check for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error('User not found');
   }
   //Getting image name from image url
-  const imageUrl=req.query.url
-  const imageUrlArray=imageUrl.split('/')
-  const image=imageUrlArray[imageUrlArray.length-1]
-  const imageName=image.split('.')[0]
-  console.log(imageName)
+  const imageUrl = req.query.url;
+  const imageUrlArray = imageUrl.split('/');
+  const image = imageUrlArray[imageUrlArray.length - 1];
+  const imageName = image.split('.')[0];
+  console.log(imageName);
   //Deleting product
- await Product.deleteOne({_id:req.query.id})
-  //Deleting image from cloudinary
-  .then(result=>{
-    cloudinary.uploader.destroy(imageName,(error,result)=>{
-      console.log(error,result)
+  await Product.deleteOne({ _id: req.query.id })
+    //Deleting image from cloudinary
+    .then((result) => {
+      cloudinary.uploader.destroy(imageName, (error, result) => {
+        console.log(error, result);
+      });
+      res.status(200).json({ message: result });
     })
-    res.status(200).json({message:result})
-
-  })
-  .catch(error=>{
-    console.log(error)
-    res.status(500).json({Error:error})
-  })
-
-})
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ Error: error });
+    });
+});
 
 //api/products/listAll
 //get req
 //public
-const listAll=asyncHandler(async(req,res)=>{
-const products=await Product.find()
-res.status(200).json({
-  success: true,
-  products,
+//Get all products of all user
+const listAll = asyncHandler(async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
 });
-})
-
 
 //api/products/single/:id
 //get req
 //public
-const singleProduct=asyncHandler(async(req,res)=>{
+//Get Single product details
+const singleProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    res.status(400)
-    throw new Error('Product not found')
+    res.status(400);
+    throw new Error('Product not found');
   }
 
   res.status(200).json({
@@ -161,4 +181,11 @@ const singleProduct=asyncHandler(async(req,res)=>{
     product,
   });
 });
-module.exports={createProduct,getProduct,editProduct,deleteProduct,listAll,singleProduct,}
+module.exports = {
+  createProduct,
+  getProduct,
+  editProduct,
+  deleteProduct,
+  listAll,
+  singleProduct,
+};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 const {createProduct,getProduct,editProduct,deleteProduct,listAll,singleProduct}=require('../controllers/product.controller')
+//Create product
 router.route('/create').post(protect,createProduct)
+//User gets all his product
 router.route('/read').get(protect,getProduct)
+//Update product
 router.route('/editProduct').put(protect,editProduct)
+//Delete product
 router.route('/deleteProduct').delete(protect,deleteProduct)
+//List all product
 router.route('/listAll').get(listAll)
+//Single product details
 router.route('/single/:id').get(singleProduct)
 module.exports = router;

0ad44f3003b6ef0cea28b851834a04dff095d1dc
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-11-19 20:23:02 +0600

    perf:#25 linked product id with each product from sellers page

--- client/components/ProductItem.js
+++ client/components/ProductItem.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-// import Link from 'next/link';
+import Link from 'next/link';
 
 const ProductItem = () => {
   const [products, setProducts] = useState([]);
@@ -8,7 +8,6 @@ const ProductItem = () => {
     fetch('http://localhost:5000/api/products/listAll', {
       headers: {
         'Content-Type': 'application/json',
-
       },
     })
       .then((response) => {
@@ -22,44 +21,50 @@ const ProductItem = () => {
   }, []);
   return (
     <div>
-       <section className="py-10 px-12">
-         <h3 className="text-left text-2xl font-semibold mb-3">
+      <section className="py-10 px-12">
+
+        <h3 className="text-left text-2xl font-semibold mb-3">
           Latest Collections
         </h3>
         {isLoading && <div>Loading... </div>}
         <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          {/* <Link href="/product/Regalia"> */}
           {products.map((product) => {
             return (
-        <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700" key={product._id}>
-          <img
-            className="rounded-t-lg object-contain object-center h-[56vh] w-full"
-            src={product.image}
-            alt="cloth image"
-          />
-          <div className="p-5">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {product.brand}
-            </h5>
-            <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">
-            <span className="font-bold">Category:</span>{' '}
-              {product.category}
-            </h5>
-            <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">
-            <span className="font-bold">Size:</span>{' '}
-           {product.size}
-            </h5>
-            <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">
-            <span className="font-bold">Price:</span>{' '}
-            {product.sellingPrice}
-            </h5>
-          </div>
-        </div>
-         );
-        })}
-      {/* </Link> */}
+              <Link href={`/product/${product._id}` }  key={product._id}>
+
+              <div
+                className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+               
+              >
+                <img
+                  className="rounded-t-lg object-contain object-center h-[56vh] w-full"
+                  src={product.image}
+                  alt="cloth image"
+                />
+                <div className="p-5">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {product.brand}
+                  </h5>
+                  <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">
+                    <span className="font-bold">Category:</span>{' '}
+                    {product.category}
+                  </h5>
+                  <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">
+                    <span className="font-bold">Size:</span> {product.size}
+                  </h5>
+                  <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">
+                    <span className="font-bold">Price:</span>{' '}
+                    {product.sellingPrice}
+                  </h5>
+                </div>
+              </div>
+              </Link>
+
+            );
+          })}
         </div>
       </section>
+
     </div>
   );
 };

5833731a6b9207c57f31aaff6ca84cb3ce8bdb17
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-12-01 09:32:50 +0600

    refactor: modified the code

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,26 +1,12 @@
-// import { useEffect, useState } from 'react'
-// import Footer from '../components/Footer'
-// import Navbar from '../components/Navbar'
-// import { useRouter } from 'next/router'
-import '../styles/globals.css'
-import '../styles/index.css'
+import '../styles/globals.css';
+import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  // const [user,setUser]=useState({value:null})
-  // const [key,setKey]=useState(0)
-  // const router=useRouter()
-  // useEffect(()=>{
-  //    const authtoken=localStorage.getItem("Token");
-  //   if(authtoken){
-  //     setUser({value:authtoken})
-  //     setKey(Math.random())
-  //   }
-  // },[router.query])
-  return <>
-  {/* <Navbar key={key} user={user}/> */}
-  <Component {...pageProps} />
-  {/* <Footer/> */}
-  </>
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;

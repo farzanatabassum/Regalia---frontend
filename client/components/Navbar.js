67184e42d400c413d90e710b85ab40dd8078e938
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-12-16 14:20:23 +0600

    ui:modified navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="bg-gray-200">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               {/* <!-- Mobile menu button--> */}
@@ -42,121 +42,127 @@ const Navbar = () => {
               </button>
             </div>
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <h1 className="px-15 py-7  text-3xl font-extrabold mr-auto">
-                Regalia
-              </h1>
-              <div className="flex flex-shrink-0 items-center">
-                <div className="hidden sm:ml-6 sm:block">
-                  <ul className="flex justify-center ">
-                    <Link href="/">
-                      <li className="ml-96 text-gray-900 px-12 py-7 rounded-md text-sm font-medium">
-                        <a>Home</a>
-                      </li>
-                    </Link>
-                    <Link href="/about">
-                      <li className="text-gray-900 hover:bg-gray-500 hover:text-white px-12 py-7 rounded-md text-sm font-medium">
-                        <a>About</a>
-                      </li>
-                    </Link>
-                    <Link href="/sellwithus">
-                      <li className="text-gray-900 hover:bg-gray-500 hover:text-white  px-12 py-7 rounded-md text-sm font-medium">
-                        <a>Sell with us</a>
-                      </li>
-                    </Link>
-                  </ul>
-                </div>
-              </div>
-              <div className=" flex  h-auto items-center px-12 py-7">
-                <a
-                  onMouseOver={() => {
-                    setDropdown(true);
-                  }}
-                  onMouseLeave={() => {
-                    setDropdown(false);
-                  }}
-                >
-                  {dropdown && (
-                    <div
-                      onMouseOver={() => {
-                        setDropdown(true);
-                      }}
-                      onMouseLeave={() => {
-                        setDropdown(false);
-                      }}
-                      className="absolute right-8 bg-gray-400 top-9 rounded-md px-5 py-4 w-44 z-10"
+              <h1 className="text-3xl font-extrabold mr-auto">Regalia</h1>
+
+              <div className="hidden sm:ml-6 sm:block">
+                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+
+                <ul className="flex space-x-4 ">
+                  <Link href="/">
+                    <li
+                      className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                      aria-current="page"
                     >
-                      <ul>
-                        <Link href={'/seller'}>
-                          <a>
-                            {' '}
-                            <li className="py-1  text-center text-base hover:bg-gray-600 rounded-md hover:text-white">
-                              My Products
-                            </li>
-                          </a>
-                        </Link>
-                        <Link href={'/updatePreference'}>
-                          <a>
-                            {' '}
-                            <li className="py-1  text-center text-base hover:bg-gray-600 rounded-md hover:text-white">
-                              My Preferences
-                            </li>
-                          </a>
-                        </Link>
-                        <Link href={'/profile'}>
-                          <a>
-                            <li className="py-1 text-base text-center hover:bg-gray-600 rounded-md hover:text-white">
-                              My Profile
-                            </li>
-                          </a>
-                        </Link>
-                        <li
-                          onClick={logout}
-                          className="py-1 text-base text-center hover:bg-gray-600 rounded-md hover:text-white"
-                        >
-                          Logout
-                        </li>
-                      </ul>
-                    </div>
-                  )}
-                  {/* User logged in */}
-                  {user.value && (
-                    <RiAccountCircleLine className="text-xl md:text-2xl mx-2" />
-                  )}
-                </a>
-                {/* User logged out */}
-                {!user.value && (
-                  <Link href="/login">
-                    <button
-                      type="button"
-                      className=" text-white bg-gray-700 font-medium rounded-lg text-sm px-5 py-3 text-center "
-                    >
-                      Sign Up/Login
-                    </button>
+                      <a>Home</a>
+                    </li>
                   </Link>
-                )}
+                  <Link href="/about">
+                    <li className="text-gray-900 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                      <a>About</a>
+                    </li>
+                  </Link>
+                  <Link href="/sellwithus">
+                    <li className="text-gray-900 hover:bg-gray-500 hover:text-white  px-3 py-2 rounded-md text-sm font-medium">
+                      <a>Sell with us</a>
+                    </li>
+                  </Link>
+                </ul>
               </div>
+            </div>
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <a
+                onMouseOver={() => {
+                  setDropdown(true);
+                }}
+                onMouseLeave={() => {
+                  setDropdown(false);
+                }}
+              >
+                {dropdown && (
+                  <div
+                    onMouseOver={() => {
+                      setDropdown(true);
+                    }}
+                    onMouseLeave={() => {
+                      setDropdown(false);
+                    }}
+                    className="absolute right-8 bg-gray-400 top-11 rounded-md px-5 py-4 w-44 z-10"
+                  >
+                    <ul>
+                      <Link href={'/seller'}>
+                        <a>
+                          {' '}
+                          <li className="py-1  text-center text-base hover:bg-gray-600 rounded-md hover:text-white">
+                            My Products
+                          </li>
+                        </a>
+                      </Link>
+                      <Link href={'/updatePreference'}>
+                        <a>
+                          {' '}
+                          <li className="py-1  text-center text-base hover:bg-gray-600 rounded-md hover:text-white">
+                            My Preferences
+                          </li>
+                        </a>
+                      </Link>
+                      <Link href={'/profile'}>
+                        <a>
+                          <li className="py-1 text-base text-center hover:bg-gray-600 rounded-md hover:text-white">
+                            My Profile
+                          </li>
+                        </a>
+                      </Link>
+                      <li
+                        onClick={logout}
+                        className="py-1 text-base text-center hover:bg-gray-600 rounded-md hover:text-white"
+                      >
+                        Logout
+                      </li>
+                    </ul>
+                  </div>
+                )}
+                {/* User logged in */}
+                {user.value && (
+                  <RiAccountCircleLine className="text-xl md:text-2xl mx-10" />
+                )}
+              </a>
+              {/* User logged out */}
+              {!user.value && (
+                <Link href="/login">
+                  <button
+                    type="button"
+                    className=" text-white bg-gray-700 font-medium rounded-lg text-sm px-5 py-3 text-center "
+                  >
+                    Sign Up/Login
+                  </button>
+                </Link>
+              )}
             </div>
           </div>
         </div>
 
+        {/* <!-- Mobile menu, show/hide based on menu state. --> */}
         <div className="sm:hidden" id="mobile-menu">
-          <div className="space-y-1 px-2 pt-2 pb-3 ">
+          <div className="space-y-1 px-2 pt-2 pb-3">
+            {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+
             <ul>
               <Link href="/">
-                <li className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
-                  Home
+                <li
+                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-base font-medium"
+                  aria-current="page"
+                >
+                  <a>Home</a>
                 </li>
               </Link>
-
               <Link href="/about">
-                <li className="text-gray-700 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                  About
+                <li className="text-gray-900 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-base font-medium">
+                  <a>About</a>
                 </li>
               </Link>
-
               <Link href="/sellwithus">
-                <li className="text-gray-700 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                  Sell with us
+                <li className="text-gray-900 hover:bg-gray-500 hover:text-white  px-3 py-2 rounded-md text-base font-medium">
+                  <a>Sell with us</a>
                 </li>
               </Link>
             </ul>

f1a71c383af2532129b2453e2e1810bb43865885
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-12-22 01:46:06 +0600

    ui:about

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import Navbar from '../components/Navbar'
-
+import Footer from '../components/Footer'
+import Image from 'next/image';
 const about = () => {
   return (
     <div>
         <Navbar/>
-      <div >
-        <h1 className='flex justify-center mt-8 text-lg font-bold'>About</h1>
-        <p className='flex items-center px-60'>
+        <div >
+        <h1 className='flex justify-center mt-8 mb-10 text-lg font-bold'>About</h1>
+        <p className='flex items-center px-72 mb-14'>
           Bonjour! Welcome to Regalia. We are open to thousands of reused branded
           clothes, where you get to choose the best fit for you, be it a casual
           wear, or a party wear based on your preference only. Buy all those
           branded pieces and uplift your style. Happy shopping!
         </p>
       </div>
+      <div className='flex justify-center mb-28'>
+      <Image
+        src="/aboutimg.png"
+        alt="homepage"
+        width={970}
+        height={250}
+      ></Image>
+
+      </div>
+       
+    
+      <Footer/>
     </div>
   );
 };
